1d80f798e7ef987d28305c88e773b571dcf997d1
Vijay Kumar <118923157+vijayvrx8055@users.noreply.github.com>
2024-07-20 23:06:15 +0530

    Initial app added

---
+++ spring-security-app/src/test/java/com/vrx/security/SpringSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrx.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
