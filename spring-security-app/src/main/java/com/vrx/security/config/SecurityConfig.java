305b1b8c1beade00309f2be8b7f589a1bd0d50e3
Vijay Kumar <118923157+vijayvrx8055@users.noreply.github.com>
2024-08-13 23:17:06 +0530

    H2 database and password encyption added

--- spring-security-app/src/main/java/com/vrx/security/config/SecurityConfig.java
+++ spring-security-app/src/main/java/com/vrx/security/config/SecurityConfig.java
@@ -1,17 +1,25 @@
 package com.vrx.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -20,22 +28,38 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableMethodSecurity //enables @PreAuthorize() - authorization for method level
 public class SecurityConfig {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests(
                 (requests) -> requests.requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated()); //any request is authenticated
-        http.sessionManagement(session->
+        http.sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(withDefaults());// configures http basic authentication with default settings
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
         return http.build();
     }
 
 
     @Bean
-    public UserDetailsService getUserDetails(){
-        UserDetails user = User.withUsername("user").password("{noop}user123").roles("USER").build();
-        UserDetails admin = User.withUsername("admin").password("{noop}admin123").roles("ADMIN").build();
-        return new InMemoryUserDetailsManager(user,admin);
+    public UserDetailsService getUserDetails() {
+        UserDetails user = User.withUsername("user")
+                .password(getPasswordEncoder().encode("user123")).roles("USER").build();
+        UserDetails admin = User.withUsername("admin")
+                .password(getPasswordEncoder().encode("admin123")).roles("ADMIN").build();
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
+        return jdbcUserDetailsManager;
+//        return new InMemoryUserDetailsManager(user,admin);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
